e4a5911d07e26c1f21704694356e9cf50084c479
Chinmay <chinmaysumant@gmail.com>
2016-11-13 22:43:48 -0500

    Redundancy

--- LinesOfAction.java
+++ LinesOfAction.java
@@ -1,9 +1,7 @@
 
 public class LinesOfAction {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         BoardFrame board = new BoardFrame();           // creates a new frame
         board.setVisible(true);                        // and shows it        
